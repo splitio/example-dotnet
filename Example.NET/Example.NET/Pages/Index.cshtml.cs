cffd73a664bffc93697cf6ca203a0ccbac318625
Mauro Sanz <mauro.sanz@split.io>
2023-05-10 11:04:16 -0300

    first commit

---
+++ Example.NET/Example.NET/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Splitio.Services.Client.Interfaces;
+
+namespace Example.NET.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string? FeatureFlagName { get; set; }
+        public string? UserKey { get; set; }
+        public string? Treatment { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ISplitClient _sdk;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            SplitioInitializer splitioInitializer)
+        {
+            _logger = logger;
+            _sdk = splitioInitializer.Sdk;
+        }
+
+        public void OnGet(string featureFlagName, string userKey)
+        {
+            if (string.IsNullOrEmpty(featureFlagName))
+            {
+                // CHANGE THIS: Copy and paste the feature flag name from Split user interface.
+                featureFlagName = "DEFAULT_FEATURE_FLAG_NAME";
+            }
+
+            if (string.IsNullOrEmpty(userKey))
+            {
+                // Change this to represent your user or account id. (usually a dynamic value)
+                userKey = "user";
+            }
+
+            Treatment = _sdk.GetTreatment(userKey, featureFlagName);
+            FeatureFlagName = featureFlagName;
+            UserKey = userKey;
+        }
+    }
+}
---
+++ Example.NET/Example.NET/SplitioInitializer.cs
@@ -0,0 +1,19 @@
+﻿using Splitio.Services.Client.Classes;
+using Splitio.Services.Client.Interfaces;
+
+namespace Example.NET
+{
+    public class SplitioInitializer
+    {
+        public ISplitClient Sdk { get; }
+
+        public SplitioInitializer()
+        {
+            var apikey = "<your SDK KEY here>";
+            var configurations = new ConfigurationOptions();
+
+            var factory = new SplitFactory(apikey, configurations);
+            Sdk = factory.Client();
+        }
+    }
+}
