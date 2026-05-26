ef8cf776415b23c9e3e00bea560487ae53edefb1
lena sano <c_lena.sano@harness.io>
2026-05-26 06:36:47 -0300

    rename class (SplitioInitializer to FmeInitializer), block SDK until ready, add visual enhancements and comments

--- Example.NET/Example.NET/Pages/Index.cshtml.cs
+++ Example.NET/Example.NET/Pages/Index.cshtml.cs
@@ -13,27 +13,31 @@ namespace Example.NET.Pages
         private readonly ISplitClient _sdk;
 
         public IndexModel(ILogger<IndexModel> logger,
-            SplitioInitializer splitioInitializer)
+            FmeInitializer fmeInitializer)
         {
             _logger = logger;
-            _sdk = splitioInitializer.Sdk;
+            _sdk = fmeInitializer.Sdk;
         }
 
         public void OnGet(string featureFlagName, string userKey)
         {
             if (string.IsNullOrEmpty(featureFlagName))
             {
-                // CHANGE THIS: Copy and paste the feature flag name from Split user interface.
+                // CHANGE THIS: Copy and paste the feature flag name from Harness FME.
                 featureFlagName = "DEFAULT_FEATURE_FLAG_NAME";
             }
 
             if (string.IsNullOrEmpty(userKey))
             {
-                // Change this to represent your user or account id. (usually a dynamic value)
-                userKey = "user";
+                // CHANGE THIS: Provide a user or account ID (usually a dynamic value). This value
+                // is used for feature flag targeting (for matching with a flag targeting rule and
+                // for percentage distribution within a targeting rule).
+                userKey = "user001";
             }
 
-            Treatment = _sdk.GetTreatment(userKey, featureFlagName);
+            Treatment = _sdk.GetTreatment(userKey, featureFlagName);    // Evaluate the feature flag for the given user or account.
+                                                                        // Feature flag impressions (evaluation info) should appear
+                                                                        // in Live tail soon after you run the demo.
             FeatureFlagName = featureFlagName;
             UserKey = userKey;
         }
